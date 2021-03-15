b771b0c268f43d6b35bde6042bc17e25bec29b52
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2021-03-14 23:47:10 -0400

    added media queries, archive tray now opens on click

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,253 @@
+import React, { useState, useLayoutEffect } from "react"
+import { motion } from "framer-motion"
+import { breakpoints } from "../../components/layout"
+import styled from "styled-components"
+
+const sidebarOpenAnimation = {
+  visible: {
+    x: "30vw",
+    transition: {
+      staggerChildren: 0.1,
+      duration: 0.35,
+      ease: "easeIn",
+    },
+  },
+  hidden: {
+    x: "96vw",
+    transition: {
+      staggerChildren: 0.1,
+      duration: 0.25,
+      ease: "easeOut",
+    },
+  },
+}
+
+const clickout = {
+  visible: {
+    visibility: "visible",
+    opacity: 1,
+    transition: {
+      delay: 0,
+      duration: 0.4,
+    },
+  },
+  hidden: {
+    visibility: "hidden",
+    opacity: 0,
+  },
+}
+
+const yearHoverEffect = {
+  hover: {
+    skew: 30,
+    color: "#5200ff",
+    transition: {
+      ease: "easeInOut",
+      duration: 0.1,
+    },
+  },
+  normal: {
+    skew: 0,
+  },
+}
+
+const Sidebar = () => {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [openSidebar, setOpenSidebar] = useState(false)
+
+  const handleClick = () => (
+    setOpenSidebar(!openSidebar), setModalOpen(!modalOpen)
+  )
+  // ----- below is a "modal" element which prevents scrolling when an archive is open -----
+
+  const useLockBodyScroll = () => {
+    useLayoutEffect(() => {
+      const originalStyle = window.getComputedStyle(document.body).overflow
+      document.body.style.overflow = "hidden"
+      return () => (document.body.style.overflow = originalStyle)
+    }, [])
+  }
+
+  const Modal = () => {
+    useLockBodyScroll()
+    return <></>
+  }
+
+  // ----- the "Past Editions" section map, which renders an archive for each year -----
+  const Years = [
+    2021,
+    2020,
+    2019,
+    2018,
+    2017,
+    2016,
+    2015,
+    2014,
+    2013,
+    2012,
+    2011,
+    2010,
+    2009,
+    2008,
+    2007,
+    2006,
+    2005,
+    2004,
+    2003,
+    2002,
+    2001,
+    2000,
+  ]
+  const allYears = Years.map(year => (
+    <Year
+      variants={yearHoverEffect}
+      whileHover="hover"
+      initial="normal"
+      onClick={handleClick}
+    >
+      {year}
+    </Year>
+  ))
+
+  return (
+    <>
+      <ArchiveYearList>
+        <h2>Past Editions</h2>
+        <YearGrid>{allYears}</YearGrid>
+      </ArchiveYearList>
+      {modalOpen && <Modal />}
+
+      <SidebarDiv
+        variants={sidebarOpenAnimation}
+        initial="hidden"
+        animate={openSidebar ? "visible" : "hidden"}
+        exit="hidden"
+      >
+        <MainContent>
+          <Header>
+            <PrevButton></PrevButton>
+            <h2>EDITION 20</h2>
+            <NextButton></NextButton>
+          </Header>
+        </MainContent>
+      </SidebarDiv>
+
+      {/* ----- empty background div to close the sidebar menu -----  */}
+      <ClickOut
+        variants={clickout}
+        initial="hidden"
+        animate={openSidebar ? "visible" : "hidden"}
+        exit="hidden"
+        onClick={handleClick}
+      />
+    </>
+  )
+}
+
+const ArchiveYearList = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 15rem;
+  padding: 10rem 0;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    rgba(234, 231, 231, 0) 68.19%,
+    rgba(255, 255, 255, 0.75) 100%
+  );
+  & h2 {
+    padding-bottom: 3rem;
+  }
+
+  @media (max-width: ${breakpoints.m}px) {
+    margin-top: 5rem;
+  }
+`
+const YearGrid = styled.div`
+  width: 45%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-auto-flow: column;
+
+  @media (max-width: ${breakpoints.m}px) {
+    width: 90%;
+    grid-auto-flow: row;
+    grid-template-columns: 1fr 1fr 1fr ;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+  }
+`
+const Year = styled(motion.div)`
+  padding: 1rem;
+  color: #636363;
+  font-family: "Space Mono", monospace;
+  font-size: 20px;
+  cursor: pointer;
+  
+  
+`
+
+const SidebarDiv = styled(motion.div)`
+  z-index: 2000;
+  position: fixed;
+  top: 0;
+  /* right: 0; */
+  background-color: #5200ff;
+
+  display: flex;
+  width: 70vw;
+  height: 100vh;
+  -webkit-box-shadow: -58px 0px 50px 58px rgba(81, 0, 255, 1);
+  -moz-box-shadow: -58px 0px 50px 58px rgba(81, 0, 255, 1);
+  box-shadow: -58px 0px 50px 58px rgba(81, 0, 255, 1);
+
+  @media (max-width: ${breakpoints.m}px) {
+
+    -webkit-box-shadow: -20px 0px 30px 10px rgba(81, 0, 255, 1);
+       -moz-box-shadow: -20px 0px 30px 10px rgba(81, 0, 255, 1);
+            box-shadow: -20px 0px 30px 10px rgba(81, 0, 255, 1);
+  }
+`
+
+const MainContent = styled.div`
+  margin: 0 auto;
+  padding-right: 10vw;
+  width: 100%;
+`
+const Header = styled.div`
+  padding-top: 3rem;
+  display: flex;
+  justify-content: space-between;
+
+  & h2 {
+    text-align: center;
+    font-size: 48px;
+    letter-spacing: 0.1rem;
+    color: white;
+    text-shadow: 0px 0px 8px #ffffff;
+  }
+`
+
+const PrevButton = styled.div`
+  width: 70px;
+  height: 70px;
+  border: 1px solid white;
+  border-radius: 50%;
+`
+const NextButton = styled.div`
+  width: 70px;
+  height: 70px;
+  border: 1px solid white;
+  border-radius: 50%;
+`
+const ClickOut = styled(motion.div)`
+  width: 100vw;
+  height: 100vh;
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+`
+
+export default Sidebar
